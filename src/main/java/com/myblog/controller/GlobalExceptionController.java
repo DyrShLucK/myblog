42a74cb01f29dff3935b4ab20f86517a61baaf98
Iduty <i.dutyshev@yandex.ru>
2025-04-23 16:55:14 +0300

    Улучшена отображение ошибок(кодов)

--- src/main/java/com/myblog/controller/GlobalExceptionController.java
+++ src/main/java/com/myblog/controller/GlobalExceptionController.java
@@ -7,6 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 @ControllerAdvice
 public class GlobalExceptionController {
@@ -14,6 +15,7 @@ public class GlobalExceptionController {
     public ModelAndView ErrorPostNotFount(PostNotFoundException e){
         ModelAndView modelAndView = new ModelAndView("error");
         modelAndView.addObject("message", e.getMessage());
+        modelAndView.addObject("statusCode", HttpStatus.NOT_FOUND.value());
         modelAndView.setStatus(HttpStatus.NOT_FOUND);
         return modelAndView;
     }
@@ -21,6 +23,7 @@ public class GlobalExceptionController {
     public ModelAndView ErrorCommentNotFount(CommentNotFoundExcepetion e){
         ModelAndView modelAndView = new ModelAndView("error");
         modelAndView.addObject("message", e.getMessage());
+        modelAndView.addObject("statusCode", HttpStatus.NOT_FOUND.value());
         modelAndView.setStatus(HttpStatus.NOT_FOUND);
         return modelAndView;
     }
@@ -28,7 +31,16 @@ public class GlobalExceptionController {
     public ModelAndView GlobalError(Exception e){
         ModelAndView modelAndView = new ModelAndView("error");
         modelAndView.addObject("message", "Непредвиденная ошибка: " + e.getMessage());
+        modelAndView.addObject("statusCode", HttpStatus.INTERNAL_SERVER_ERROR.value());
         modelAndView.setStatus(HttpStatus.INTERNAL_SERVER_ERROR);
+        return modelAndView;
+    }
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public ModelAndView handleNoHandlerFound(NoHandlerFoundException ex) {
+        ModelAndView modelAndView = new ModelAndView("error");
+        modelAndView.addObject("message", "Страница не найдена: " + ex.getRequestURL());
+        modelAndView.addObject("statusCode", HttpStatus.NOT_FOUND.value());
+        modelAndView.setStatus(HttpStatus.NOT_FOUND);
         return modelAndView;
     }
 }
